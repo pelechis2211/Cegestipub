032e322f17702a03c770d479b69892399dc5a9d5
Pablo Luna <pabloluna2211@gmail.com>
2024-10-13 22:29:11 -0500

    Agregando el archivo index.html y las carpetas (css, imagenes, js)de la página principal

---
+++ js/script.js
@@ -0,0 +1,83 @@
+/*Header - Menu*/
+
+window.onscroll = function() {
+
+    scroll = document.documentElement.scrollTop;
+
+    header = document.getElementById("header");
+
+    if(scroll > 20){
+        header.classList.add('nav_mod');
+        header.style.backgroundColor = 'rgb(250, 253, 245, 0.75)';
+    }else if(scroll < 20){
+        header.classList.remove('nav_mod');
+        header.style.backgroundColor = 'transparent';
+    } 
+}
+
+/*Menu hamburger*/
+const hamburger = document.getElementById('hamburger');
+    const header = document.querySelector('header');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        header.classList.toggle('active');
+});
+
+
+/*Nuestra misión y visión*/
+
+function toggleAccordion(element) {
+    const content = element.nextElementSibling;
+    const isOpen = content.style.display === "block";
+
+    if (isOpen) {
+        content.style.display = "none";
+        element.querySelector('.arrow').textContent = '⬇';
+        element.classList.remove('active');
+    } else {
+        content.style.display = "block";
+        element.querySelector('.arrow').textContent = '⬆';
+        element.classList.add('active');
+    }
+}
+
+
+
+/*Programa de Estudios*/
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    slidesPerGroup: 3,
+    loop: true,
+    centerSlider: true,
+    fade: true,
+    grabCursor: true,
+    //loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clicKable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 4,
+        },
+    },
+});
+
+
+
+
